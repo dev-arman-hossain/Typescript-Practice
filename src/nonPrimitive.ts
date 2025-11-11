f2e2b4da9ed79109f56f362b357845b8c7b1a9b0
Arman Hossain <web.devarman@gmail.com>
2025-11-11 23:52:09 +0600

    -array, tuple

--- src/nonPrimitive.ts
+++ src/nonPrimitive.ts
@@ -7,10 +7,17 @@ someThings.push("ethical");
 console.log(someThings); 
 
 
+
+
 // TS: tuple
 
 let coordinated: [number, number] = [20,30]
 
+let couple: [string, string] = ["Husband", "wife"];
+
+let ArmanNameRoll: [string, number] = ["Arman", 45];
+
+let destination: [string, string, number] = ["Dhaka", "Chittagong", 150];
 
 // reference type : object
 
